1c4bcc2328960e4845dcffd09784908410484472
blacknon <blacknon@orebibou.com>
2017-12-16 22:19:57 +0900

    change stdout.ReadBytes delim

--- pty/run.go
+++ pty/run.go
@@ -43,7 +43,7 @@ func (t *Terminal) Start(c *exec.Cmd) (err error) {
 	go func() {
 		for {
 			time.Sleep(10)
-			by, _ := stdout.ReadBytes(('\x0a' | '\x23' | '\x24' | '\x25'))
+			by, _ := stdout.ReadBytes(('\x0a' | '\x23' | '\x24' | '\x25' | '\x22' | '\x27' | '\x7b' | '\x7d'))
 
 			t.Tty.Write(by)
 			if t.Log != nil {
